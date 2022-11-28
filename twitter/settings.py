ccd66b0d8adb8977b22242abcc3aba801c12b682
pbajie <56032286+pbajie@users.noreply.github.com>
2022-11-28 09:30:23 -0800

    add to git7(chapter14)

--- twitter/settings.py
+++ twitter/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'accounts',
+    'tweets',
 ]
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
